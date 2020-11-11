748bd0d66628213363a549bf7667ccfb59be1455
ishandudeja <ishandudeja@gmail.com>
2020-11-11 18:23:03 +1300

    unique pasword function update

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -18,16 +18,16 @@ const initialState: UserState = {
 
         },
     ],
-     newPas: {  }
+    newPas: {}
 }
 
 const reducer = (
     state: UserState = initialState,
     action: UserAction
 ): UserState => {
-    
+
     switch (action.type) {
-        
+
         case actionTypes.ADD_USER:
             debugger
             const newUser: IUser = {
@@ -40,41 +40,41 @@ const reducer = (
                 users: state.users.concat(newUser),
             }
         case actionTypes.GENERATE_PASWORD:
-           let validateNum: (num:Number)=> any  =function (num:Number):any{
-               let isValid:boolean=true;
-               if(num.toString().length<4)
-               isValid=false;
-             let pin:string[]=   num.toString().split('')
-             if (pin[0]+pin[1]==pin[2]||pin[1]+pin[2]==pin[3])
-             isValid=false  
+            let validateNum: (num: Number) => any = function (num: Number): any {
+                let isValid: boolean = true;
+                if (num.toString().length < 4)
+                    isValid = false;
+                let pin: string[] = num.toString().split('')
+                if (pin[0] + pin[1] == pin[2] || pin[1] + pin[2] == pin[3])
+                    isValid = false
 
-             if(0== Number( pin[0])-Number( pin[1]) ||Number( pin[1])-Number( pin[2])==0 || Number( pin[2])-Number( pin[3]) )
-             isValid=false;
-             if (!isValid)
-            return validateNum(Math.round( Math.random()*10000))
-            else
-            return num
-            
+                if (0 == Number(pin[0]) - Number(pin[1]) || Number(pin[1]) - Number(pin[2]) == 0 || Number(pin[2]) - Number(pin[3]))
+                    isValid = false;
+                if (!isValid)
+                    return validateNum(Math.round(Math.random() * 10000))
+                else
+                    return num
 
-           }
 
-            let genPasword: () => IPasword = function ( ): IPasword {
-                const pwd:IPasword={ 
-                        s1: validateNum( Math.round( Math.random()*10000)), // not really unique
-                        s2: Math.round(Math.random()*10000),
-                        s3: Math.round( Math.random()*10000),
-                        s4:Math.round( Math.random()*10000),
-                        s5:Math.round( Math.random()*10000)
-                       }
-                       state.newPas=pwd
-                       return pwd;
-                      
-              };
-              genPasword();
-            
+            }
+
+            let genPasword: () => IPasword = function (): IPasword {
+                const pwd: IPasword = {
+                    s1: validateNum(Math.round(Math.random() * 10000)), // not really unique
+                    s2: validateNum(Math.round(Math.random() * 10000)),
+                    s3: validateNum(Math.round(Math.random() * 10000)),
+                    s4: validateNum(Math.round(Math.random() * 10000)),
+                    s5: validateNum(Math.round(Math.random() * 10000))
+                }
+                state.newPas = pwd
+                return pwd;
+
+            };
+            genPasword();
+
             return {
                 ...state
-                
+
             }
         case actionTypes.REMOVE_USER:
             const updatedUser: IUser[] = state.users.filter(
@@ -84,19 +84,19 @@ const reducer = (
                 ...state,
                 users: updatedUser,
             }
-            case actionTypes.UPDATE_USER:
-                debugger
-            const updateIndex = state.users.findIndex( obj => obj.id == action.user.id )
-            const updatedObj = { ...state.users[updateIndex], name: action.user.name};
-            const updatedUsers:IUser[] = [
+        case actionTypes.UPDATE_USER:
+            debugger
+            const updateIndex = state.users.findIndex(obj => obj.id == action.user.id)
+            const updatedObj = { ...state.users[updateIndex], name: action.user.name };
+            const updatedUsers: IUser[] = [
                 ...state.users.slice(0, updateIndex),
                 updatedObj,
                 ...state.users.slice(updateIndex + 1),
-              ];
-              console.log(updatedUsers);
+            ];
+            console.log(updatedUsers);
             return {
                 ...state,
-                 users: updatedUsers,
+                users: updatedUsers,
             }
     }
     return state
