0d0ea518040a56ecd6591c40abba574eabf44784
ishandudeja <ishandudeja@gmail.com>
2020-11-11 17:37:43 +1300

    define User list Component and define router functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
+import { Users } from "./components/Users"
+import { AddUser } from "./components/AddUser"
+import { addUser, genPwd, removeUser } from "./store/actionCreators"
+import { Dispatch } from "redux"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
+  const users: readonly IUser[] = useSelector(
+    (state: UserState) => state.users,
+    shallowEqual
+  )
+
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const saveUser = React.useCallback(
+    (user: IUser) => dispatch(addUser(user)),
+    [dispatch]
+  )
+
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-light">
+      <Router>
+        <nav className="nav shadow-sm bg-white">
+
+          <Link to="/" className="nav-link active" >Generate</Link>
+          <Link to="/users" className="nav-link " >Saved</Link>
+        </nav>
+        <div className="container">
+          <Route path="/" exact >
+            <AddUser saveUser={saveUser}  />
+          </Route>
+          <Route path="/users" exact >
+            {users.map((user: IUser) => (
+              <Users
+                key={user.id}
+                user={user}
+                removeUser={removeUser}
+              />
+            ))}
+
+            {users.length==0? <div>No User List Available</div> :"" }
+
+          </Route>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Users.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import { Dispatch } from "redux"
+import { useDispatch } from "react-redux"
+import { updateUser } from "../store/actionCreators"
+type Props = {
+  user: IUser
+  removeUser: (user: IUser) => void
+}
+
+export const Users: React.FC<Props> = ({ user, removeUser }) => {
+  const dispatch: Dispatch<any> = useDispatch()
+  const [emp, setEmp] = React.useState<IUser | {}>()
+  const deleteUser = React.useCallback(
+    (user: IUser) => dispatch(removeUser(user)),
+    [dispatch, removeUser]
+  )
+  React.useEffect(function effectFunction() {
+    if (emp) {
+      dispatch(updateUser(user))
+    }
+  }, [emp]);
+  const handleUserData = (user: IUser, e: React.FormEvent<HTMLInputElement>) => {
+    user.name = e.currentTarget.value
+    setEmp(user)
+      }
+
+
+
+  return (
+    <div className="container m-2">
+      <div className="d-flex justify-content-center ">
+      
+          <input type="text" value={user.name} className="m-1 border border-secondary p-2 bg-white" onChange={(e) => handleUserData(user, e)} id="name" />
+          <div className="m-1 border border-secondary p-2 bg-white"   >{user.pasword?.s1}</div>
+          <div className="m-1 border border-secondary p-2 bg-white"   >{user.pasword?.s2}</div>
+          <div className="m-1 border border-secondary p-2 bg-white"   >{user.pasword?.s3}</div>
+          <div className="m-1 border border-secondary p-2 bg-white"   >{user.pasword?.s4}</div>
+          <div className="m-1 border border-secondary p-2 bg-white"   >{user.pasword?.s4}</div>
+          <div className="p-2"> <button className="btn btn-danger btn-sm" onClick={() => deleteUser(user)}>Delete</button></div>
+          
+        </div>
+
+      
+
+    </div>
+
+  )
+}
