0a6780b794a7d186b6fff1c3cc5eebeb605a61f8
ishandudeja <ishandudeja@gmail.com>
2020-11-14 17:47:31 +1300

    update reducer test

--- src/store/reducer.test.ts
+++ src/store/reducer.test.ts
@@ -17,20 +17,52 @@ const initialState: UserState = {
     ],
     newPas: {}
 }
-describe('todos reducer', () => {
+const user: IUser = { id: 1, name: "name", pasword: {} }
+describe('User reducer', () => {
     it('should return the initial state', () => {
         expect(reducer(undefined, {})).toEqual(initialState)
-    })
+    });
 
     it('should handle ADD_USER', () => {
-const user:IUser={ id: 0, name: "name", pasword: {} }
 
+        //add new user and check the lenght of user list
         expect(
-            reducer( initialState, {
+            reducer(initialState, {
                 type: types.ADD_USER,
                 user
-            })
-        ).toEqual(initialState.users.concat(user))
-    })
+            }).users.length
+        ).toEqual(initialState.users.length + 1)
+    });
+
+    it('should handle Gentate Pasword', () => {
+
+        //generate new pasword
+        expect(
+            reducer(initialState, {
+                type: types.GENERATE_PASWORD,
+                user
+            }).newPas
+        ).not.toBe({})
+    });
+
+    it('should handle Remove User', () => {
+        //remove user and check the lenght of user list
+        expect(
+            reducer(initialState, {
+                type: types.REMOVE_USER,
+                user
+            }).users.length
+        ).toBe(initialState.users.length - 1)
+    });
+
+    it('should handle Update User', () => {
+        let updateUser: IUser = { id: 1, name: "Ishan", pasword: {} }
+        expect(
+            reducer(initialState, {
+                type: types.UPDATE_USER,
+                 user:updateUser
+            }).users.find(f=>f.id=updateUser.id)?.name
+        ).toEqual("Ishan")
+    });
 })
 
